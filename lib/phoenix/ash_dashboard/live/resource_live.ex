e251dbe675c56bfd4563df257e8f08061dbaa97e
Andrew Callahan <andrewcallahan@users.noreply.github.com>
2020-05-04 18:57:52 -0500

    grid working

--- lib/phoenix/ash_dashboard/live/resource_live.ex
+++ lib/phoenix/ash_dashboard/live/resource_live.ex
@@ -39,165 +39,116 @@ defmodule AshDashboard.ResourceLive do
   @impl true
   def render(assigns) do
     ~L"""
-      single resource 1
-      @resource
-      <%= Ash.name(@resource) %>
+      <h2><%= Ash.name(@resource) %> Resource</h2>
       <%= live_redirect("Back", to: ash_dashboard_path(@socket, :resources), class: "menu-item") %>
 
-      
       <%= f = form_for @resource, "#", [phx_change: :validate, phx_submit: :save, class: "resource"] %>
-
-      <section class="info">
-        <h5>Info</h5>
-        <div class="group">
-          <%= label f, :name %>
-          <input value="<%= @resource.name() %>">
-        </div>
-
-        <div class="group">
-          <%= label f, :type %>
-          <input value="<%= @resource.type() %>">
-        </div>
-
-        <div class="group">
-          <%= label f, :max_page_size %>
-          <input value="<%= @resource.max_page_size() %>" type="number">
-        </div>
-
-        <div class="group">
-          <%= label f, :default_page_size %>
-          <input value="<%= @resource.default_page_size() %>" type="number">
-        </div>
-
-        <div class="group">
-          <%= label f, :primary_key %>
-            <input value="<%= List.first(@resource.primary_key()) |> Atom.to_string() %>">
+        <div class="form-row">
+          <div class="form-group col-sm-4">
+            <label for="inputName4">Name</label>
+            <input type="text" class="form-control" id="inputName4" placeholder="Name" value="<%= @resource.name() %>">
           </div>
-      </section>
-
-      <section class="attributes">
-        <h5>Attributes</h5>
+          <div class="form-group col-sm-2">
+            <label for="inputType4">Type</label>
+            <select name="node" class="custom-select" id="node-select">
+              <option value="nonode@nohost" selected="">String</option>
+              <option value="nonode@nohost" selected="">Integer</option>
+              <option value="nonode@nohost" selected="">Boolean</option>
+              <option value="nonode@nohost" selected="">Date</option>
+            </select>
+          </div>
+          <div class="form-group col-sm-2">
+            <label for="inputPrimaryKey4">Primary Key</label>
+            <input type="text" class="form-control" id="inputPrimaryKey4" placeholder="PrimaryKey" value="<%= @resource.type() %>">
+          </div>
+          <div class="form-group col-sm-2">
+            <label for="inputMaxPageSize4">Max Page Size</label>
+            <input type="text" class="form-control" id="inputMaxPageSize4" placeholder="MaxPageSize" value="<%= @resource.max_page_size() %>">
+          </div>
+          <div class="form-group col-sm-2">
+            <label for="inputDefaultPageSize4">Default Page Size</label>
+            <input type="text" class="form-control" id="inputDefaultPageSize4" placeholder="DefaultPageSize" value="<%= @resource.default_page_size() %>">
+          </div>
+        </div>        
+        <div class="form-row">
+          <div class="form-group col-sm-3">
+            <label for="inputName4">Name</label>
+          </div>
+          <div class="form-group col-sm-2">
+          <label for="inputType4">Type</label>
+          </div>
+          <div class="form-group col-sm-1">
+            <label for="inputName4">Allow Nil?</label>
+          </div>
+          <div class="form-group col-sm-1">
+            <label for="inputName4">2</label>
+          </div>
+          <div class="form-group col-sm-1">
+            <label for="inputName4">3</label>
+          </div>
+          <div class="form-group col-sm-1">
+            <label for="inputName4">4</label>
+          </div>
+          <div class="form-group col-sm-1">
+            <label for="inputName4">5</label>
+          </div>
+          <div class="form-group col-sm-1">
+            <label for="inputName4">6</label>
+          </div>
+          <div class="form-group col-sm-1">
+            <label for="inputName4">7</label>
+          </div>
+        </div>
         <%= for attribute <- @resource.attributes do %>
-          <div class="group">
-            <%= label f, attribute.name %>
-            <input>
-          </div>
-        <% end %>
-      </section>
-
-      <section class="actions">
-        <h5>Actions</h5>
-        <%= for action <- @resource.actions do %>
-          <div class="group">
-            <%= label f, action.name %>
-            <input>
-          </div>
-          <div class="group">
-            <%= label f, action.type %>
-            <input>
-          </div>
-
-          <%= if action.type == :read do %>
-            <div class="group">
-              <%= label f, action.paginate? %>
-              <input type="checkbox">
+          <div class="form-row">
+            <div class="form-group col-sm-3">
+              <input type="text" class="form-control" id="inputName4" placeholder="Name" value="<%= attribute.name() %>">
             </div>
-          <% end %>
-    
-          <div class="group">
-            <%= label f, action.primary? %>
-            <input type="checkbox">
+            <div class="form-group col-sm-2">
+              <select name="node" class="custom-select" id="node-select">
+                <option value="nonode@nohost" selected="">String</option>
+                <option value="nonode@nohost" selected="">Integer</option>
+                <option value="nonode@nohost" selected="">Boolean</option>
+                <option value="nonode@nohost" selected="">Date</option>
+              </select>
+            </div>
+            <div class="form-group col-sm-1">
+              <input type="checkbox" id="gridCheck">
+            </div>
+            <div class="form-group col-sm-1">
+              <input type="checkbox" id="gridCheck">
+            </div>
+            <div class="form-group col-sm-1">
+              <input type="checkbox" id="gridCheck">
+            </div>
+            <div class="form-group col-sm-1">
+              <input type="checkbox" id="gridCheck">
+            </div>
+            <div class="form-group col-sm-1">
+              <input type="checkbox" id="gridCheck">
+            </div>
+            <div class="form-group col-sm-1">
+              <input type="checkbox" id="gridCheck">
+            </div>
+            <div class="form-group col-sm-1">
+              <input type="checkbox" id="gridCheck">
+            </div>
           </div>
-
-          <%= for rule <- action.rules do %>
-            RULE!
-          <% end %>
         <% end %>
-      </section>
-    </form>
-
+      </form>
     """
   end
+
+  @impl true
+  def handle_event("validate", _, socket) do
+    IO.inspect('validate event')  
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("save", _, _csrf_token, socket) do
+    IO.inspect('validate event')  
+    {:noreply, socket}
+  end
+
 end
-
-# <h2><%= @title %></h2>
-
-# <%= f = form_for @changeset, "#",
-#   id: "resource-form",
-#   phx_target: @myself,
-#   phx_change: "validate",
-#   phx_submit: "save" %>
-
-#   <%= label f, :name %>
-#   <%= text_input f, :name %>
-#   <%= error_tag f, :name %>
-
-#   <%= label f, :type %>
-#   <%= text_input f, :type %>
-#   <%= error_tag f, :type %>
-
-#   <%= label f, :max_page_size %>
-#   <%= number_input f, :max_page_size %>
-#   <%= error_tag f, :max_page_size %>
-
-#   <%= label f, :default_page_size %>
-#   <%= number_input f, :default_page_size %>
-#   <%= error_tag f, :default_page_size %>
-
-#   <%= label f, :primary_key %>
-#   <%= checkbox f, :primary_key %>
-#   <%= error_tag f, :primary_key %>
-
-#   <h1>Attributes</h1>
-
-#   <%= inputs_for f, :attributes, fn i -> %>
-#     <div class="form-group">
-#       <%= label i, :name, "Bar ##{i.index + 1}", class: "control-label" %>
-#       <%= text_input i, :name, class: "form-control" %>
-#     </div>
-#   <% end %>
-
-#   <%= submit "Save", phx_disable_with: "Saving..." %>
-# </form>
-
-
-# <%= for resource <- @resources do %>
-# <%= Ash.name(resource) %>
-
-# <%= f = form_for resource, "#", [phx_change: :validate, phx_submit: :save, class: "form-inline"] %>
-
-#   <%= label f, :name %>
-#   <%= text_input f, :name %>
-
-#   <%= label f, :type %>
-#   <%= text_input f, :type %>
-
-#   <br>
-
-#   <%= label f, :max_page_size %>
-#   <%= number_input f, :max_page_size %>
-
-#   <%= label f, :default_page_size %>
-#   <%= number_input f, :default_page_size %>
-
-#   <%= label f, :primary_key %>
-#   <%= checkbox f, :primary_key %>
-
-#   <%= submit "Save", phx_disable_with: "Saving..." %>
-# </form>
-# <br>
-# <br>
-# <% end %>
-
-
-# @impl true
-# def handle_event("validate", _, socket) do
-#   IO.inspect('validate event')  
-#   {:noreply, socket}
-# end
-
-# @impl true
-# def handle_event("save", _, _csrf_token, socket) do
-#   IO.inspect('validate event')  
-#   {:noreply, socket}
-# end
